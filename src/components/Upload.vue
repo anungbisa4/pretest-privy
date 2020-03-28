a8812a0b1049d089c988a349989501f000f8c366
anungbisa4 <anung.bisa4@gmail.com>
2020-03-29 03:16:38 +0700

    feature-upload-single

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,20 +1,48 @@
 <template>
   <div>
-    <q-dialog v-model="alert">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Alert</div>
-        </q-card-section>
+  <q-uploader
+      url="http://localhost:8080/uploads"
+      label="Upload your images"
+      :factory="factory"
+      :multiple="multiple"
+    >
+      <template v-slot:list="scope">
+        <q-list separator>
+          <q-item v-for="file in scope.files" :key="file.name">
+            <q-item-section>
+              <q-item-label class="full-width ellipsis">
+                {{ file.name }}
+              </q-item-label>
 
-        <q-card-section class="q-pt-none">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
-        </q-card-section>
+              <q-item-label caption>
+                {{ file.__sizeLabel }}
+              </q-item-label>
+            </q-item-section>
 
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+            <q-item-section
+              v-if="file.__img"
+              thumbnail
+              class="gt-xs"
+            >
+              <img :src="file.__img.src">
+            </q-item-section>
+
+            <q-item-section top side>
+              <q-btn
+                class="gt-xs"
+                size="12px"
+                flat
+                dense
+                round
+                icon="delete"
+                @click="scope.removeFile(file)"
+              />
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+      </template>
+      </q-uploader>
   </div>
 </template>
 
@@ -22,8 +50,9 @@
 export default {
   name: 'upload',
   props: {
-    status: Boolean
-
+    status: Boolean,
+    factory: Function,
+    multiple: Boolean
   },
   data () {
     return {
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,17 +2,17 @@
   <q-page class="flex dashboard">
     <q-card class="dashboard-card">
       <div class="dashboard-cover">
-          <q-icon class="icon-edit" name="create"></q-icon>
+          <q-icon class="icon-edit" name="create" @click="isUploadSingle = true"></q-icon>
         <div class="dashboard-cover-img">
-          <img class="dashboard-cover-img__image" src="statics/cover-sample.jpeg" alt="">
+          <img class="dashboard-cover-img__image" :src="dataProfile && dataProfile.user_picture && dataProfile.cover_picture.url || 'statics/cover-sample.jpeg'" alt="">
         </div>
       </div>
       <div class="flex d-avatar">
         <div class="d-avatar-middle--photo">
           <div class="d-avatar-wrap">
-            <q-icon class="icon-edit d-avatar-icon__bottom" name="create"></q-icon>
+            <q-icon class="icon-edit d-avatar-icon__bottom" name="create" @click="isUploadMultiple = true, isUserPicture = true"></q-icon>
             <button>
-              <img class="d-avatar-img__image" src="statics/avatar-sample.jpeg" alt="">
+              <img class="d-avatar-img__image" :src="dataProfile && dataProfile.user_picture && dataProfile.user_picture.picture.url" alt="">
             </button>
           </div>
         </div>
@@ -149,54 +149,21 @@
         </div>
       </section>
     </q-card>
-    <q-dialog v-model="isUploadMultiple">
+    <q-dialog v-model="isUploadMultiple" @hide="isUserPicture = false">
       <q-card>
         <div class="q-pa-md">
-      <q-uploader
-      url="http://localhost:8080/uploads"
-      label="Upload your images"
-      :factory="factoryFn"
-      multiple
-    >
-      <template v-slot:list="scope">
-        <q-list separator>
-          <q-item v-for="file in scope.files" :key="file.name">
-            <q-item-section>
-              <q-item-label class="full-width ellipsis">
-                {{ file.name }}
-              </q-item-label>
-
-              <q-item-label caption>
-                {{ file.__sizeLabel }}
-              </q-item-label>
-            </q-item-section>
-
-            <q-item-section
-              v-if="file.__img"
-              thumbnail
-              class="gt-xs"
-            >
-              <img :src="file.__img.src">
-            </q-item-section>
-
-            <q-item-section top side>
-              <q-btn
-                class="gt-xs"
-                size="12px"
-                flat
-                dense
-                round
-                icon="delete"
-                @click="scope.removeFile(file)"
-              />
-            </q-item-section>
-          </q-item>
-
-        </q-list>
-      </template>
-    </q-uploader>
-          </div>
-
+        <Upload :factory="factoryFn" :multiple="isUserPicture ? false: true"/>
+        </div>
+        <q-card-actions align="right">
+          <q-btn flat label="Close" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-dialog v-model="isUploadSingle">
+      <q-card>
+        <div class="q-pa-md">
+        <Upload :factory="factoryFnSingle" :multiple="false"/>
+        </div>
         <q-card-actions align="right">
           <q-btn flat label="Close" color="primary" v-close-popup />
         </q-card-actions>
@@ -207,12 +174,15 @@
 
 <script>
 // eslint-disable-next-line no-unused-vars
+import '../css/pages/dashboard.scss'
 import { Loading, LocalStorage, Notify } from 'quasar'
 // eslint-disable-next-line no-unused-vars
 import { objectToFormData } from 'object-to-formdata'
+import Upload from 'src/components/Upload'
 import axios from 'axios'
 export default {
   name: 'login',
+  components: { Upload },
   data () {
     return {
       test: '',
@@ -220,6 +190,8 @@ export default {
       isEdit: false,
       isEditCareer: false,
       isUploadMultiple: false,
+      isUploadSingle: false,
+      isUserPicture: false,
       educationForm: {
         school_name: '',
         company_name: ''
@@ -240,6 +212,25 @@ export default {
     console.log(this.$store)
   },
   methods: {
+    factoryFnSingle (file) {
+      return new Promise((resolve, reject) => {
+        this.$axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+        const fileData = new FormData()
+        fileData.append('image', file[0])
+        const response = this.$axios.post('/api/v1/uploads/cover', fileData)
+        response.then((res) => {
+          if (res.status === 201) {
+            this.isUploadSingle = false
+            this.initProfile()
+            Notify.create({
+              type: 'positif',
+              message: 'Upload Succesfully'
+            })
+          }
+        })
+        resolve(response)
+      })
+    },
     factoryFn (file) {
       return new Promise((resolve, reject) => {
         this.$axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
@@ -249,11 +240,26 @@ export default {
         response.then((res) => {
           if (res.status === 201) {
             this.isUploadMultiple = false
-            this.initProfile()
-            Notify.create({
-              type: 'positif',
-              message: 'Upload Succesfully'
-            })
+            if (this.isUserPicture) {
+              this.isUserPicture = false
+              this.$axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+              const response = this.$axios.post('/api/v1/uploads/profile/default', 'id=' + res.data.data.user_picture.id)
+              response.then((res) => {
+                if (res.status === 201) {
+                  this.initProfile()
+                  Notify.create({
+                    type: 'positif',
+                    message: 'Upload Succesfully'
+                  })
+                }
+              })
+            } else {
+              this.initProfile()
+              Notify.create({
+                type: 'positif',
+                message: 'Upload Succesfully'
+              })
+            }
           }
         })
         resolve(response)
@@ -334,262 +340,3 @@ export default {
   }
 }
 </script>
-<style lang="stylus" scoped>
-// 1px 8px 16px rgba(0, 0, 0, .08)
-  .dashboard {
-    justify-content: center;
-    .icon-edit {
-      z-index: 1;
-      background-color: #5458F7;
-      color: #ffffff;
-      padding: 10px;
-      border-radius: 50%;
-      position: absolute;
-      right: 10px;
-      top: 10px;
-    }
-    .d-img-overlay{
-      position: absolute;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      z-index: 1;
-      background: #00000061;
-    }
-    .d-img-blur{
-      position: absolute;
-      top: 0;
-      right: 0;
-      left: 0;
-      bottom: 0;
-      background-size: cover;
-      -webkit-filter: blur(2px);
-      filter: blur(2px);
-    }
-    .d-btn {
-      background-color: #5458F7;
-      color: #ffffff;
-      font-size: 12px;
-      text-transform: capitalize;
-      &.d-btn__outline {
-        color : #5458F7;
-      }
-      &.d-btn__grey {
-        color : #9597A1;
-      }
-      &.d-btn-margin {
-        margin:30px;
-      }
-    }
-    .d-title {
-      color : #9597A1;
-      font-size: 12px;
-    }
-    .dashboard-card {
-      margin : 20px;
-      width : 100%;
-      border-radius: 20px;
-      padding-bottom: 50px;
-      box-shadow : 0px 0px 5px rgba(0, 0, 0, 0.2),
-                  0 2px 2px rgba(0, 0, 0, 0),
-                  0 10px 1px -2px rgba(0, 0, 0, 0);
-      .dashboard-cover {
-        position : relative;
-        margin: 0;
-        padding: 0;
-        border: 0;
-        font-size: 100%;
-        vertical-align: baseline;
-        background: transparent;
-        width: 100%;
-        padding-bottom: 25%;
-        .dashboard-cover-img {
-          border-top-left-radius: 20px;
-          border-top-right-radius: 20px;
-          position: absolute;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          overflow: hidden;
-        }
-        .dashboard-cover-img__image {
-          width: 100%;
-          height : 100%;
-        }
-      }
-      .d-avatar {
-        position : relative;
-        justify-content: center;
-        margin-top: -80px;
-        .d-avatar-icon__bottom {
-          bottom: 5px;
-          left: 50%;
-          transform: translateX(-50%);
-        }
-        .d-avatar-middle--photo {
-          width: 160px;
-          height: 160px;
-          box-sizing: border-box;
-          background-clip: content-box;
-          border: 0 solid transparent;
-          border-radius: 49.9%;
-          background-color: rgba(243,246,248,.94);
-          box-shadow: inset 0 1.5px 3px 0 rgba(0,0,0,.15), 0 1.5px 3px 0 rgba(0,0,0,.15);
-          margin: auto;
-          position: relative;
-          .d-avatar-wrap {
-            background-color: transparent;
-            display: block;
-            width: 100%;
-            height: 100%;
-            border-radius: 50%;
-            margin: 0 auto;
-            position: relative;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            box-sizing: border-box;
-            button {
-              background: none;
-              border: none;
-              padding: 0;
-              line-height: 0;
-              margin: 0;
-              width: 100%;
-              height: 100%;
-            }
-            .d-avatar-img__image {
-              border-radius: 50%;
-              width: 152px;
-              height: 152px;
-            }
-          }
-        }
-      }
-      .d-profile {
-        position: relative;
-        text-align : center;
-        padding : 10px;
-        .d-profile-abs {
-          position : absolute;
-          right: 30px;
-        }
-        .d-profile-pd {
-          padding: 0 5px;
-        }
-        h1 {
-          font-size: 20px;
-        }
-        h2 {
-          font-size: 14px;
-          line-height: normal;
-          text-align: center;
-          margin: 0;
-          color : #9597A1;
-        }
-      }
-      .d-about {
-        width: 50%;
-        margin: 0 auto;
-        background-color: #ececee;
-        border-radius: 16px;
-        padding: 10px 20px;
-        font-size: 12px;
-      }
-      .d-card {
-        position : relative;
-        width: 50%;
-        margin: 30px auto 0 auto;
-        background-color: #ffffff;
-        border-radius: 16px;
-        padding: 10px 20px;
-        font-size: 12px;
-        box-shadow : 1px 8px 16px rgba(0, 0, 0, .08);
-        .d-card-btn {
-          position : absolute;
-          right: 30px;
-          bottom: -15px;
-        }
-        .d-card-item {
-          margin: 0 10px;
-          align-items: center;
-          &:not(:last-child) {
-          border-bottom: 1px solid #f4f4f4;
-           }
-            .d-card-item__avatar {
-              position: relative;
-              width: 40px;
-              height: 40px;
-              background: #cdf4e9;
-              color: #00CC99;
-              text-transform: uppercase;
-              border-radius: 50px;
-              span {
-                position : absolute;
-                left: 50%;
-                top : 50%;
-                transform : translate(-50%, -50%);
-              }
-            }
-            .d-card-item__content {
-              padding: 10px;
-              h1 {
-                line-height : normal;
-                font-size: 14px;
-                font-weight: 300;
-              }
-              span {
-                font-size : 12px;
-                font-weight: 300;
-                color : #9597A1;
-              }
-            }
-        }
-      }
-      .d-career {
-        .d-career-avatar {
-          background-color : #eae5ff !important;
-          color : #B2A1FF !important;
-        }
-        h2 {
-          font-size: 12px;
-          margin: 0;
-          line-height: normal;
-          color: #9597A1 !important;
-        }
-      }
-      .d-gallery {
-        .col-4 {
-          padding: 1px;
-          position : relative;
-        }
-        .d-gallery-item {
-          height: 200px;
-          position: relative;
-          background: #f8f9fa;
-          img {
-              max-height: 100%;
-              max-width: 100%;
-              width: auto;
-              height: auto;
-              position: absolute;
-              top: 0;
-              bottom: 0;
-              left: 0;
-              right: 0;
-              margin: auto;
-          }
-        }
-        .d-gallery-btn {
-            text-align: center;
-            padding: 20px;
-          button {
-            padding: 0 25px;
-          }
-        }
-      }
-    }
-  }
-</style>
